f7ff1101505926c6ee0cf2c6435646ea0ee5c337
Valera <treiserboobern19@gmail.com>
2022-02-11 19:15:01 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 12000;
-        int mile = 20;
-        int bonus = price / mile;
-        System.out.println(bonus);
+        int balance = 100;
+        int replenishment;
+        int limit = 1000;
+        int bonus;
+
+        replenishment = 10000;
+        if(replenishment > limit) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+        int total = balance + replenishment + bonus;
+
+        System.out.println(total);
     }
 }
