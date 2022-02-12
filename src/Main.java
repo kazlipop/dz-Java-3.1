5c7d246401182694bdd20e52948ce4eb4d369e53
kazlipop <88086013+kazlipop@users.noreply.github.com>
2022-02-12 23:55:45 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 100;
-        int replenishment;
-        int limit = 1000;
-        int bonus;
-
-        replenishment = 10000;
-        if(replenishment > limit) {
-            bonus = replenishment / 100;
-        } else {
-            bonus = 0;
-        }
-        int total = balance + replenishment + bonus;
-
-        System.out.println(total);
+        int price = 12000;
+        int mile = 20;
+        int bonus = price / mile;
+        System.out.println(bonus);
     }
 }
